5dd803fb00bbc5d233f3065daa6012dc833213b7
Ekin Koluk <ekinkoluk@gmail.com>
2021-11-21 20:20:37 +0000

    post request and twit char counter

---
+++ public/scripts/composer-char-count.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+  const charLimit = 140;
+  $('#tweet-text').on('input', function() {
+    const charLeft = charLimit - $(this).val().length;
+    if (charLeft < 0) {
+      $(this).parentsUntil(".new-tweet")
+        .find(".counter")
+        .addClass('counterMinus');
+    } else {
+      $(this).parentsUntil(".new-tweet")
+        .find(".counter")
+        .removeClass('counterMinus');
+    }
+    $(this).parentsUntil(".new-tweet")
+      .find(".counter")
+      .text(charLeft);
+  });
+});
--- server/index.js
+++ server/index.js
@@ -35,3 +35,8 @@ app.use("/tweets", tweetsRoutes);
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
+app.post("/tweets", (req,res)=> {
+  const text = req.body.text;
+  return res.redirect('./public/index.html');
+})
